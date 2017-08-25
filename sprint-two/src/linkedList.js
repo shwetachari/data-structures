06de75154f4a45efbc4a20253d8a2ea11f560f83
shwetachari <shwetachari@gmail.com>
2017-08-24 21:19:40 -0700

    Wrote Class and functional methods for linkedList data structure

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -3,13 +3,43 @@ var LinkedList = function() {
   list.head = null;
   list.tail = null;
 
+  list.size = 0;
+
   list.addToTail = function(value) {
+    var newNode = Node(value);
+    if (list.size === 0) {
+      list.head = newNode;
+      list.tail = newNode;
+    }
+    list.tail.next = newNode;
+    list.tail = newNode;
+    list.size++;
   };
 
   list.removeHead = function() {
+    var removedHead = list.head;
+    list.head = removedHead.next;
+    list.size--;
+    return removedHead.value;
   };
 
   list.contains = function(target) {
+    var checkNext = function(node) {
+      //base case: node.value === target
+        //return true;
+      //base case: node.next === null
+        //return false
+      //recursive case:
+        //return checkNext(node.next);
+      if (node.value === target) {
+        return true;
+      } else if (node.next === null) {
+        return false;
+      } else {
+        return checkNext(node.next);
+      }
+    };
+    return checkNext(list.head);
   };
 
   return list;
@@ -26,4 +56,7 @@ var Node = function(value) {
 
 /*
  * Complexity: What is the time complexity of the above functions?
+  .addToTail = O(1)
+  .removeHead = O(1)
+  .contains = O(n)
  */
